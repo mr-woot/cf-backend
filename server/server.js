05ee2470932d8a8147076d285d0f4bcdb49c9574
mr-woot <tusharmudgal3@gmail.com>
2017-05-23 02:08:22 +0530

    cors fixes

--- server/server.js
+++ server/server.js
@@ -5,6 +5,19 @@ let consign = require('consign');
 
 let app = express();
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+     // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+});
+
 consign()
   .include('./server/config')
   .then('./server/helpers')
